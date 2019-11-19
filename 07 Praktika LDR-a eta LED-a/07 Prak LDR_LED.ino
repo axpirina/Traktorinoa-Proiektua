7f083e003b6d8daf8cfc35b991b40db2fc46be8c
axpirina <axpirina@oteitzalp.com>
2019-11-19 17:40:32 +0100

    Create 07 Prak LDR_LED.ino

---
+++ 07 Praktika LDR-a eta LED-a/07 Prak LDR_LED.ino
@@ -0,0 +1,29 @@
+/*
+Arduino batekin ingurunean dugun argi kantitatea neurtu eta LED bat proportzioan pizten duen programa.
+* By TheCircuit
+* Modified by Axpi 2019
+* Creatives Commons
+*/
+ 
+int argiValue = 0; // Aldagaien deklarazioa
+int led = 11; 
+  
+void setup() {
+ 
+// Serie komunikazioa martxan jarri
+Serial.begin(9600);
+}
+ 
+void loop() {
+ 
+argiValue = analogRead(A0);           // 0 pin analogikoa irakurri eta aldagaiean gorde
+//argiValue = map (argiValue, ARGI MINIMOA,ARGI MAXIMOA,0,255);   // Sentsorea kalibratzeko ondorengoak bistaratu
+//argiValue = constrain(argiValue,0,255);
+Serial.print("Gelako argiaren balioa 1-255 artean: ");    // Irakurririko balioa inprimatu
+Serial.println(argiValue); 
+
+int ledValue = map (argiValue, 0,255,255,0); 
+analogWrite(led,ledValue);
+
+delay(150);
+}
