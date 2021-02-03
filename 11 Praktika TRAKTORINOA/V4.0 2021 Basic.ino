6f3e8fc6fd35d88a0999b5bbd1d7d041031c5dcd
axpirina <axpirina@oteitzalp.com>
2021-02-03 12:42:55 +0100

    Create V4.0 2021 Basic.ino

---
+++ 11 Praktika TRAKTORINOA/V4.0 2021 Basic.ino
@@ -0,0 +1,24 @@
+#include <Traktorinoa.h>
+#include<UltraDistSensor.h>
+
+Traktorinoa niretraktorinoa;
+UltraDistSensor mysensor;
+
+float reading;
+
+void setup() {
+  Serial.begin(9600);
+  mysensor.attach(7, 8); //Trigger pin , Echo pin
+}
+
+void loop() {
+  reading = mysensor.distanceInCm();
+  Serial.print("Sensor Reading :"); Serial.print(reading); Serial.println(" ZM");
+  if (reading<20){
+     niretraktorinoa.atzera();
+    }
+  else{
+
+  niretraktorinoa.aurrera();
+  }
+}
