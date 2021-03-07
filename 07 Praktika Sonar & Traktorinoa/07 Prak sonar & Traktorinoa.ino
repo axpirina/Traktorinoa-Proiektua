11f0aa351a7acbdc27435c83afa56656be1b8312
axpirina <axpirina@oteitzalp.com>
2021-03-07 22:11:04 +0100

    Update 07 Prak sonar & Traktorinoa.ino

--- 07 Praktika Sonar & Traktorinoa/07 Prak sonar & Traktorinoa.ino
+++ 07 Praktika Sonar & Traktorinoa/07 Prak sonar & Traktorinoa.ino
@@ -1,29 +1,66 @@
-/* L298P Motor Shield-a eta Traktorino.h liburutegia erabiliaz sakatzaile bat sakatzen denean traktorinoa aurrerantz mugiarazten duen programa. 
-  Begiratu traktorinoa.h liburutegiaren dokumentazioa eskuragarri dituzuen beste funtzio guztiak ezagutzeko.
-          https://github.com/axpirina/Traktorinoa
-  2020 ko Abenduan by Axpi
+/* L298P Motor Shield-a eta Traktorino.h liburutegia eta HC-SR04 sentsorea erabiliaz sakatzaile bat sakatzen denean traktorinoa aurrerantz 
+mugiaraztuko da 25zm azpitik oztoporen bat aurkitu biartean. Orduan atzera egingo du.  
+
+  Debounce metodoa aplikatu zaio sakatzaileari.
+  created 21 Nov 2006 by David A. Mellis
+  modified 20 sep 2018 by Axpi
   This example code is in the public domain.
+  http://www.arduino.cc/en/Tutorial/Debounce
 */
 
 #include <Traktorinoa.h>
-
-int balioa = 0;
+#include<UltraDistSensor.h>
 Traktorinoa niretraktorinoa;
+UltraDistSensor mysensor;
+float reading;
+
+// Aldagaiak definitu
+const int buttonPin = 2;
+int State = LOW; // LED-aren hasierako balioa
+int buttonState; // Sakatzailearen oraingo egoera
+int lastButtonState = LOW; // Sakatzailearen aurreko egoera
+
+// DEBOUNCE denborak.
+unsigned long lastDebounceTime = 0; // Lehen aldiz sakatua
+unsigned long debounceDelay = 50; // Debounce denbora.
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // Serie komunikazioa martxan jarri.
+  pinMode(buttonPin, INPUT);
+  mysensor.attach(7, 8); //Trigger pin , Echo pin
+
+  Serial.print("Sakatzailea sakatu zain...........");
 }
 
 void loop() {
-  
-  balioa = digitalRead(2);
-  Serial.print("Sakatzailea: ");
-  Serial.println(balioa);
-  
-  if ( balioa == 1 ){
-    niretraktorinoa.aurrera();
+  int reading = digitalRead(buttonPin);
+  if (reading != lastButtonState) { //Sakatzailea sakatu
+    lastDebounceTime = millis();   //Timerra inizializatu
   }
-  else{
-    niretraktorinoa.stop();
+  // Botoiaren egoera aldatu baldin bada:
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {  // Botoia aldatu bada
+      buttonState = reading; // Balioak eguneratu
+      if (buttonState == HIGH) {//LED-a HIGH bada soilik
+        State = !State;
+        Serial.print("Egoera: ");
+        Serial.println(State);
+      }
+    }
+  }
+  lastButtonState = reading; // Balioak eguneratu
+  reading = mysensor.distanceInCm();
+  Serial.print("Sensor Reading :"); Serial.print(reading); Serial.println(" ZM");
+
+  if ( State == 1 ) {
+    niretraktorinoa.aurrera();
+
+    while (reading > 1 && reading < 25) {
+      niretraktorinoa.atzera();
+      reading = mysensor.distanceInCm();
+    }
+  }
+  else {
+    niretraktorinoa.geratu();
   }
 }
