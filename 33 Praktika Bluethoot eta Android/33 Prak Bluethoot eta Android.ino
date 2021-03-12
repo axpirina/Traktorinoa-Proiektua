9e645cc56641ebe1e062e4fc93ead756754d8e54
axpirina <axpirina@oteitzalp.com>
2021-03-12 10:34:29 +0100

    Update 33 Prak Bluethoot eta Android.ino

--- 33 Praktika Bluethoot eta Android/33 Prak Bluethoot eta Android.ino
+++ 33 Praktika Bluethoot eta Android/33 Prak Bluethoot eta Android.ino
@@ -1,30 +1,27 @@
 /*  Bluethoot hargailua (SPP-C HC-06) erabiliaz Arduinoa haririk gabe kontrolatuko duen 
  *  programa da hurrengoa.
  *  
- *  13. Pinean konektatua duen LED-a kontrolatuko dugu praktika honetan baina kode hau
- *  Traktorino proiektu osoa gomandatzeko ere erabil daiteke.
- *  
  *  Android bitartez kontrolatzeko erabiliko dugun aplikazioa "Arduino bluetooth controller"
  *  izango da. https://play.google.com/store/apps/details?id=com.giumig.apps.bluetoothserialmonitor&hl=en
  *  
  *  OHARRA: Programa igotzerako garaian Bluethoot gailua DESKONEKTATU !!!
  *  
- *  2019 ko Azaroan by Axpi
+ *  2021 ko Otsaila by Axpi
  *  Eskerrak https://create.arduino.cc/projecthub/SURYATEJA/bluetooth-control-leds-27edbd
  *  Eskerrak http://www.martyncurrey.com/bluetooth-modules/#SPP-C_HC-06-BT06_HC-06
  *  This example code is in the public domain.
  */
-// int E1 = 10; //Motorraren Abiadura PWM.
-// int M1 = 12; // Motorraren Norantza.
-// int piezoPin = 4;
 
-int led = 13;
+#include <Traktorinoa.h>
+#include<UltraDistSensor.h>
+Traktorinoa niretraktorinoa;
+UltraDistSensor mysensor;
+float reading;
 
 void setup()
 {
-  //pinMode(M1, OUTPUT);
-  pinMode(led, OUTPUT);
   Serial.begin(9600);
+  mysensor.attach(7, 8); //Trigger pin , Echo pin
 }
 
 void loop() {
@@ -33,10 +30,18 @@ void loop() {
     char data = Serial.read(); // bluetooth modulutik datorren datua irakurri
     switch (data)
     {
-      case 'a': digitalWrite(13, HIGH); break; // Datuaren balioaren arabera aukera desberdinak egin
-      case 'b': digitalWrite(13, LOW); break; 
+      case 'a': niretraktorinoa.aurrera(); break; 
+      case 'b': niretraktorinoa.atzera(); break; 
+      case 'c': niretraktorinoa.ezkerrera(); break; 
+      case 'd': niretraktorinoa.eskuinera(); break; 
+      case 'p': niretraktorinoa.geratu(); break;  
       default : break;
     }
   }
   delay(50);
+  reading = mysensor.distanceInCm();
+  while (reading >1 && reading <25){
+    niretraktorinoa.atzera();
+    reading = mysensor.distanceInCm();
+    }
 }
