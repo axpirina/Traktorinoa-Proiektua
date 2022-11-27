bdb2f78f656e28789283ff9ce4e83ae81f35a572
axpirina <axpirina@oteitzalp.com>
2022-11-27 19:46:27 +0100

    Update 34 Prak LDR_LED.ino

--- 34 Praktika LDR-a eta LED-a/34 Prak LDR_LED.ino
+++ 34 Praktika LDR-a eta LED-a/34 Prak LDR_LED.ino
@@ -18,14 +18,12 @@ pinMode(led,OUTPUT);
 void loop() {
  
 argiValue = analogRead(A0);           // 0 pin analogikoa irakurri eta aldagaiean gorde
-//argiValue = map (argiValue, ARGI MINIMOA,ARGI MAXIMOA,0,255);   // Sentsorea kalibratzeko ondorengoak bistaratu
+//argiValue = map (argiValue,ARGI MAXIMOA,ARGI MINIMOA,0,255);   // Sentsorea kalibratzeko ondorengoak bistaratu
 //argiValue = constrain(argiValue,0,255);
 Serial.print("Gelako argiaren balioa 1-255 artean: ");    // Irakurririko balioa inprimatu
 Serial.println(argiValue); 
 
-int ledValue = argiValue;
-ledValue = map (ledValue, 0,255,255,0); 
-analogWrite(led,ledValue);
+analogWrite(led,argiValue);
 
 delay(150);
 }
